e2e975acf0948e6ee7fbd632ae517f6e6d4ffc32
CypriotUnknown <torosilgaz@gmail.com>
2025-03-28 23:13:00 +0200

    added unmarshal function

--- models/models.go
+++ models/models.go
@@ -1,6 +1,18 @@
 package models
 
+import "encoding/json"
+
 type EncryptedBody struct {
 	IV   string `json:"iv"`
 	Hash string `json:"hash"`
+}
+
+func UnmarshalEncryptedBody(bytes []byte) (*EncryptedBody, error) {
+	var body EncryptedBody
+	err := json.Unmarshal(bytes, &body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &body, nil
 }
