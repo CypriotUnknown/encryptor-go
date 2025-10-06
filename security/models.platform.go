5256f534d162af3ea108eb9d0947119c713f5724
CypriotUnknown <torosilgaz@gmail.com>
2025-10-06 11:54:02 +0300

    added ExtractPlatformFromString

--- security/models.platform.go
+++ security/models.platform.go
@@ -6,3 +6,16 @@ const (
 	PlatformBrowser Platform = "browser"
 	PlatformApp     Platform = "app"
 )
+
+var platforms map[string]Platform = map[string]Platform{
+	string(PlatformBrowser): PlatformBrowser,
+	string(PlatformApp):     PlatformApp,
+}
+
+func ExtractPlatformFromString(s string) *Platform {
+	if platform, exists := platforms[s]; exists {
+		return &platform
+	}
+
+	return nil
+}
